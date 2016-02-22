b596a960550f66338062271fb5445c504d3e483f
Tyler Nakamura <tyler@tylernakamura.com>
2016-02-21 15:37:09 -1000

    add base for interface

--- Interface.java
+++ Interface.java
@@ -1,7 +1,39 @@
+import java.util.Scanner; 
+
 public class Interface 
 {
 	public static void main(String [] args)
 	{
-		System.out.println("Hello World!");
+		String userInput = "";
+		Scanner userInputScanner = new Scanner(System.in);
+		boolean exitProgram = false;
+
+		//get user input
+		while(!exitProgram)
+		{
+			//take user input and convert to lower case
+			System.out.println("Please enter a command!\nFor a list of commands, type \"commands\"");
+       		userInput = userInputScanner.nextLine();
+       		userInput = userInput.toLowerCase();
+
+       		if(userInput.equals("commands"))
+       		{
+       			System.out.println("You have entered the \"Commands\" button!");
+       			exitProgram = true;
+       		}
+       		else
+       		{
+       			System.out.println("I am not sure what you mean, perhaps type \"Commands\"?");
+       		}
+		}
+	}
+
+	/*
+		Returns all possible commands that a user could enter
+	*/
+	private void printAllCommands()
+	{
+		System.out.println("-------COMMANDS--------");
+
 	}
 }
