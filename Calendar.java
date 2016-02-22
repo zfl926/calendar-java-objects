7670ac07629c265eca9bec1135a90443a596c9da
Tyler Nakamura <tyler@tylernakamura.com>
2016-02-21 16:53:14 -1000

    add shims to Calendar.java

--- Calendar.java
+++ Calendar.java
@@ -1,21 +1,32 @@
 public class Calendar
-{
-	String outputName = "outputcalendar.ics";
+{	
+	//file must be in same working directory as java program
+	String fileName = "outputcalendar.ics";
 
 	//default constructor
 	public Calendar()
 	{
-		System.out.println("A calendar has been made");	
+		System.out.println("A calendar has been made, output will be stored with the name: " + fileName);	
 	}
 
 	//overloaded constructor
-	public Calendar(String filename)
+	public Calendar(String fileName)
 	{
-
+		this.fileName = fileName;
 	}
 
 	public void importIcs()
 	{
 
+	}
+
+	public void exportIcs()
+	{
+
+	}
+
+	public void addEvent()
+	{
+		
 	}
 }
