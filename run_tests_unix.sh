2ecb33f193abdac30b86090a575cc30726dcd62f
Tyler Nakamura <tyler@tylernakamura.com>
2016-02-25 09:13:07 -1000

    check to make sure file size is not 0

--- run_tests_unix.sh
+++ run_tests_unix.sh
@@ -2,7 +2,9 @@
 #note: https://github.com/TylerNakamura/Basic-JUnit-Template
 
 input_file="testInput.ics"
-if [ -e "$input_file" ]
+
+#input_file exists and is greater than 0 size
+if [ -e "$input_file" ] && [ -s "$input_file" ]
 then
 #compile and run tests
 javac -cp .:junit-4.12.jar UnitTests.java
@@ -10,6 +12,6 @@ java -cp .:junit-4.12.jar:hamcrest-core-1.3.jar org.junit.runner.JUnitCore UnitT
 else
 #qualifications to run tests aren't met and response is printed
 printf "\n"
-echo "Please make sure that a $input_file exists before running tests!"
+echo "Please make sure that a $input_file exists with valid data before running tests!"
 printf "\n"	
 fi
