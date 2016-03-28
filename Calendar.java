ec2f10fbe977e0b56cbfbeb3ee34cadd8a0174dc
Tyler Nakamura <tyler@tylernakamura.com>
2016-03-27 20:35:46 -1000

    add stubs for sorting and calculating the great circle distance

--- Calendar.java
+++ Calendar.java
@@ -225,4 +225,20 @@ public class Calendar
 			System.out.println(e);
 		}
 	}
+
+	//TODO
+	//sorts the events in the calendar by the start date
+	public void sortCalendar()
+	{
+		//sort that ish here :D
+	}
+
+	//TODO
+	//prints the results of the great circle distance
+	//DO NOT SORT HERE, SORTING SHOULD BE DONE BY THE SORT METHOD
+	//instead, just calculate the gcd by whatever the order the events are in, sorting should no be in this method
+	public void printGreatCircleDistance()
+	{
+		//calculate dakine here
+	}
 }
