09b68a14bf26fbdf2cfb784372da0c8bd3d20f90
Tyler Nakamura <tyler@tylernakamura.com>
2016-03-28 15:17:47 -1000

    fix script

--- test.sh
+++ test.sh
@@ -4,11 +4,11 @@
 input_file="testInput.ics"
 
 #input_file exists and is greater than 0 size
-if [ -e "$input_file" ] && [ -s "$input_file" ] 
+if [ -e "$input_file" ] && [ -s "$input_file" ]
 then
   #compile and run tests
   javac -cp .:junit-4.12.jar UnitTests.java
-  returnVal = $(java -cp .:junit-3.12.jar:hamcrest-core-1.3.jar org.junit.runner.JUnitCore UnitTests)
+  returnVal = $(java -cp .:junit-4.12.jar:hamcrest-core-1.3.jar org.junit.runner.JUnitCore UnitTests)
   echo "Here is the return value: "
   echo $returnVal
 else
