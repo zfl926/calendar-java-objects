c00fe1e1c59cda1b46dc5f9ec68c481bc6ba73a7
Tyler Nakamura <tyler@tylernakamura.com>
2016-04-07 18:29:27 -1000

    Coordinate is now a inner class of Geo

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-javac Calendar.java Interface.java Vevent.java Coordinate.java Geo.java RandomString.java
+javac Calendar.java Interface.java Vevent.java Geo.java RandomString.java
 java Interface
